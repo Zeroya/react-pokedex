5a8313d782c97fe42015b9e1e7f97c8205504ae9
Yan Latyshev <yan.latyshev@gmail.com>
2022-02-17 21:15:39 +0200

    added hover card

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,7 +8,10 @@ const Sprite = styled.img`
 
 const Card = styled.div`
 box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-background-color:{name};
+transition:all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+&:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
 `;
 
 export default class PokemonCard extends Component {
