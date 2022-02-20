60a656edde8a0cab2917f5181850e8dc0aad425b
Yan Latyshev <yan.latyshev@gmail.com>
2022-02-20 22:28:55 +0200

    some added

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -6,13 +6,31 @@ export default class Pokemon extends Component {
   state = {
     name: '',
     pokemonIndex: '',
-    imageUrl: ''
+    imageUrl: '',
+    types: [],
+    decription: '',
+    stats: {
+      hp: "",
+      attack: "",
+      defence: "",
+      speed: "",
+      specialAttack: "",
+      specialDefense: ""
+    },
+    height: "",
+    weight: "",
+    eggGroup: "",
+    abilities: '',
+    genderRatioMale: '',
+    genreRatioFemale: "",
+    evs: "",
+    hatchSteps: ""
   };
 
   async componentDidMount() {
-    
+
     const { pokemonIndex } = this.props.match.params;
-    
+
 
     const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
     const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
@@ -20,13 +38,62 @@ export default class Pokemon extends Component {
     const pokemonRes = await axios.get(pokemonUrl);
 
     const name = pokemonRes.data.name;
+    const imageUrl = pokemonRes.data.sprites.front_default;
 
-    this.setState({ name })
+    let [hp, attack, defense, speed, specialAttack, specialDefense] = '';
+
+    pokemonRes.data.stats.map(stat => {
+      switch (stat.stat.name) {
+        case 'hp':
+          hp = stat['base_stat']
+          break;
+        case 'attack':
+          attack = stat['base_stat']
+          break;
+        case 'defense':
+          defense = stat['base_stat']
+          break;
+        case 'speed':
+          speed = stat['base_stat']
+          break;
+        case 'special-attack':
+          specialAttack = stat['base_stat']
+          break;
+        case 'special-defense':
+          specialDefense = stat['base_stat']
+          break;
+
+      }
+    })
+
+    const height = Math.round((pokemonRes.data.height * 0.328084 + 0.001) * 100) / 100;
+    const weight = Math.round((pokemonRes.data.weight * 0.220462 + 0.001) * 100) / 100; // convert to pounds 
+
+    const types = pokemonRes.data.types.map(type => type.type.name);
+
+    const abilities = pokemonRes.data.abilities.map(abilities => {
+      return abilities.ability.name.toLowerCase().split('-').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(" ");
+    })
+
+    const evs = pokemonRes.data.stats.filter(stat => {
+      if (stat.effort > 0) {
+        return true;
+      }
+      return false;
+      
+    }).map(stat => {
+      return `${stat.efort} ${stat.stat.name}`
+      .toLowerCase().split('-').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(" ");
+    }).join(", ");
+
+    this.setState({ name, imageUrl })
   }
 
   render() {
-    return (
+    return (<div>
       <div>{this.state.name}</div>
+      <div>{this.state.imageUrl}</div>
+    </div>
     )
   }
 }
